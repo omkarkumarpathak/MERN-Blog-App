067ddb68d5a7f6332ef742a935040ed71b7170c0
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2024-06-07 17:20:08 +0530

    Created Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 function App() {
 
   return (
-    <>
-      <h1>omkar</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/sign-up" element={<SignUp/>}></Route>
+      </Routes>
+
+    </Router>
   )
 }
 
