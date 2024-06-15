e572350ba6eeb5d0ade1d0711af9a8bfa7cc3de6
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2024-06-15 17:27:53 +0530

    Added OAuth Functionality with backend

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ9CLZRBgxcchrVmvk5cPwSZC-iSIrBYk",
+  authDomain: "mern-blog-app-caea8.firebaseapp.com",
+  projectId: "mern-blog-app-caea8",
+  storageBucket: "mern-blog-app-caea8.appspot.com",
+  messagingSenderId: "432319333528",
+  appId: "1:432319333528:web:e6dbcb0ff0e0ad3e3f7ced",
+  measurementId: "G-RE8P1XQ2EG"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store, persistor } from './redux/store.js'
-import {Provider} from 'react-redux'
-import {PersistGate} from 'react-persist/integration/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-    <App />
-  </Provider>,
+        <App />
+    </Provider>
   </PersistGate>
-)
+);
