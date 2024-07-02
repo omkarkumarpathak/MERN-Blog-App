393ca6ed9e9887ccb72ee6791e6481ab91bfadc1
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2024-07-02 17:26:27 +0530

    Update user profile api

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -7,18 +7,29 @@ import {
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
-
 import  {app}  from '../../firebase.js';
+
+import { updateStart,updateFailure,updateSuccess } from '../redux/user/userSlice.js';
+import { useDispatch } from 'react-redux';
 
 export default function DashProfile() {
 
   const { currentUser } = useSelector((state) => state.user);
+
   const [imagFile, setImageFile] = useState(null);
   const [imageFileURL, setImageFileURL] = useState(null);
+  
+   const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
   const filePickRef = useRef();
+  const dispatch=useDispatch();
 
   const [imageFileUploadProgress,setImageFileUploadProgress]=useState(null);
   const [imageFileUploadError,setImageFileUploadError]=useState(null);
+
+  //for update profile
+  const [formData,setFormData]=useState({});
+
   console.log(imageFileUploadProgress);
 
   const handleImageChange = (e) => {
@@ -29,6 +40,51 @@ export default function DashProfile() {
     }
 
   }
+
+  const handleChange=(e)=>{
+    setFormData({...formData, [e.target.id]:e.target.value})
+  }
+
+  console.log(formData);
+
+  function formValidation(){
+    
+  }
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError('No changes made');
+      return;
+    }
+
+    formValidation();
+
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
+    }
+  };
+
   useEffect(()=> {
 
     const uploadImage=async()=>{
@@ -56,6 +112,7 @@ export default function DashProfile() {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
             
             setImageFileURL(downloadURL);
+            setFormData({...formData, profilePicture:downloadURL});
           });
         }
       )
@@ -65,30 +122,35 @@ export default function DashProfile() {
     if(imagFile) uploadImage();
 
   }, [imagFile])
+
   return (
     <div className='w-[70%] p-5 mx-auto'>
 
       <h1 className='text-3xl font-semibold text-center'>Profile</h1>
 
-      <form className='mt-10 flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='mt-10 flex flex-col gap-4'>
+       
         <input type="file" accept='image/*'
           onChange={handleImageChange}
           ref={filePickRef}
-          hidden />
-        <div className='w-32 h-32 rounded-full mx-auto
-        '>
+          hidden 
+        />
+
+        <div className='w-32 h-32 rounded-full mx-auto'>
+
           <img alt="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png"
-            src={imageFileURL || currentUser.profilePicture} className='rounded-full w-full h-full border-8 
+            src={ imageFileURL || currentUser.profilePicture  } className='rounded-full w-full h-full border-8 
           shadow-md  cursor-pointer'
-            onClick={() => filePickRef.current.click()} />
+            onClick={() => filePickRef.current.click()}
+           />
 
         </div>
 
         {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
 
-        <TextInput type='text' placeholder={currentUser.username} ></TextInput>
-        <TextInput type='text' placeholder={currentUser.email} ></TextInput>
-        <TextInput type='password' placeholder='password' ></TextInput>
+        <TextInput id='username' type='text' placeholder={currentUser.username} onChange={handleChange} ></TextInput>
+        <TextInput id='email' type='text' placeholder={currentUser.email}  onChange={handleChange}></TextInput>
+        <TextInput id='password' type='password' placeholder='password'  onChange={handleChange}></TextInput>
         <Button type='submit' gradientDuoTone='purpleToPink'>Update</Button>
 
         <div className='flex justify-between flex-nowrap'>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,35 +1,47 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+
+
 export default function SignUp() {
+
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
   const handleSubmit = async (e) => {
+
     e.preventDefault();
+
     if (!formData.username || !formData.email || !formData.password) {
       return setErrorMessage('Please fill out all fields.');
     }
+    
     try {
       setLoading(true);
       setErrorMessage(null);
+
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
-      });
+      }); 
+
       const data = await res.json();
       if (data.success === false) {
         return setErrorMessage(data.message);
       }
+
       setLoading(false);
       if(res.ok) {
         navigate('/sign-in');
       }
+
     } catch (error) {
       setErrorMessage(error.message);
       setLoading(false);
